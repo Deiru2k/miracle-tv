815cb1df275fe0bf1f4ed835facc1a1333de314c
auctumnus <auctumnus@users.noreply.github.com>
2021-08-23 10:06:21 -0500

    refactor: use `chalk` for colored logs

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,6 +3,8 @@ import { graphqlEndpoint } from "miracle-tv-server/graphql";
 import { setupDB } from "miracle-tv-server/db/setup-db";
 import { graphqlUploadExpress } from "graphql-upload";
 import config from "miracle-tv-server/config";
+import { green } from "chalk";
+
 
 // app.get("/", (_, res) => res.send("FUCK!"));
 // app.use(graphqlUploadExpress());
@@ -21,14 +23,11 @@ const main = async () => {
     config.server?.hostname || "0.0.0.0",
     () => {
       console.info(
-        "\x1b[32m",
-        `Server started on ${config.server?.hostname || "0.0.0.0"}:${
+        green`Server started on ${config.server?.hostname || "0.0.0.0"}:${
           config.server?.port || 4000
-        }`,
-        "\x1b[0m"
+        }`
       );
       console.info(
-        "",
         `Playground: http://${config.domain || "localhost"}:${
           config.server?.port || 4000
         }/graphql`
