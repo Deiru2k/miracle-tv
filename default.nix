d59917f8d0d4e5f71f552e597ec7005c1149d44b
Dale TwoKey <deiru2k@gmail.com>
2021-08-25 02:13:20 +0300

    fix: Working default.nix

--- default.nix
+++ default.nix
@@ -22,28 +22,26 @@ in mkYarnPackage rec {
   '';
 
   buildPhase = ''
-    yarn build:server
     yarn build:client
   '';
 
   installPhase = ''
     mkdir -p $out/server
-    cp -R ./dist/server/* $out/server/
-    mkdir -p $out/server/graphql
-    cp -R src/server/graphql/schema/* $out/server/graphql/
-    makeWrapper ${nodePkg}/bin/node $out/bin/server \
-      --add-flags "$out/server/index.js" \
-      --set node_modules $node_modules \
+    mkdir -p $out/bin
+    cp -R ./* $out/
+    makeWrapper $out/node_modules/.bin/ts-node $out/bin/server \
+      --add-flags "-r tsconfig-paths/register" \
+      --add-flags "$out/src/server/server.ts" \
+      --set NODE_ENV production \
       --set NODE_PATH $NODE_PATH:$node_modules
 
     mkdir -p $out/client/dist
     cp -R ./next-production.config.js $out/client/next.config.js
     cp -R ./dist/client/* $out/client/dist
-    makeWrapper $node_modules/.bin/next $out/bin/client \
+    makeWrapper $out/node_modules/.bin/next $out/bin/client \
       --add-flags "start" \
       --add-flags "$out/client" \
-      --set node_modules $node_modules \
-      --set NODE_PATH $NODE_PATH:$node_modules
+      --set NODE_PATH $out/node_modules:$NODE_PATH
   '';
 
   distPhase = ''
