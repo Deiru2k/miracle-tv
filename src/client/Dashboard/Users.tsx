58fda457ef58e582d3ae12fa824f77acbca80091
Dale TwoKey <deiru2k@gmail.com>
2022-01-03 23:03:43 +0300

    feat: Empty profile directory

--- src/client/Dashboard/Users.tsx
+++ src/client/Dashboard/Users.tsx
@@ -1,10 +1,12 @@
 import { gql } from "@apollo/client";
-import { SimpleGrid } from "@chakra-ui/layout";
+import { SimpleGrid, Text } from "@chakra-ui/layout";
+import { Button } from "@chakra-ui/react";
 import { Link } from "miracle-tv-client/components/ui/Link";
 import { UserPanel } from "miracle-tv-client/components/ui/users/UserPanel";
 import { MediaQuery } from "miracle-tv-client/utils/const";
 import { useMediaQuery } from "miracle-tv-client/utils/css";
 import { useUsersDirectoryQuery } from "miracle-tv-shared/hooks";
+import React from "react";
 
 export const USER_DIRECTORY_FRAGMENT = gql`
   fragment UserDirectoryProfile on User {
@@ -40,18 +42,34 @@ export const DashboardUserDirectory = (): any => {
   const isMobile = useMediaQuery(MediaQuery.mobile);
   const { data: { userDirectory = [] } = {} } = useUsersDirectoryQuery();
   return (
-    <SimpleGrid columns={isMobile ? 2 : 4} spacing={4}>
-      {userDirectory.map((user) => (
-        <Link key={user.id} href={`/user/${user.username}`}>
-          <UserPanel
-            user={user}
-            _hover={{
-              textDecoration: "none",
-            }}
-            includeDescription={false}
-          />
-        </Link>
-      ))}
-    </SimpleGrid>
+    <>
+      {!userDirectory.length && (
+        <Text>
+          No users in the directory... be the first to{" "}
+          <Link
+            as={(props) => (
+              <Button py={0} px={1} variant="ghost" mr={1} {...props} />
+            )}
+            href="/settings/user/preferences"
+          >
+            [feature yourself]
+          </Link>
+          ?
+        </Text>
+      )}
+      <SimpleGrid columns={isMobile ? 2 : 4} spacing={4}>
+        {userDirectory.map((user) => (
+          <Link key={user.id} href={`/user/${user.username}`}>
+            <UserPanel
+              user={user}
+              _hover={{
+                textDecoration: "none",
+              }}
+              includeDescription={false}
+            />
+          </Link>
+        ))}
+      </SimpleGrid>
+    </>
   );
 };
