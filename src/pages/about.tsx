a940a0bac25f9a00e284ee7c7bb45799d2307071
Dale TwoKey <deiru2k@gmail.com>
2022-01-04 00:16:08 +0300

    feat: Slightly better about page

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 
 import { Page } from "miracle-tv-client/components/system/Page";
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Text, Heading, Image } from "@chakra-ui/react";
+import { Link } from "miracle-tv-client/components/ui/Link";
 
 const AboutPage = () => {
   return (
     <Page>
-      <Flex>
-        <Heading>About</Heading>
+      <Flex justify="center" direction="column" align="center">
+        <Image src="/images/sanae_gyate.png" w="20vw" h="20vw" />
+        <Heading>Welcome to Miracle TV!</Heading>
+        <Heading size="md">An open source streaming suite.</Heading>
+        <Text as="span" size="md">
+          <Link href="/auth/login">[Login]</Link> or{" "}
+          <Link href="/auth/sign-up">[Sign up]</Link> to start using this
+          instance.
+        </Text>
       </Flex>
     </Page>
   );
